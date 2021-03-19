7ec94ed53ab60de3762d09c4d4bffe14138bac8f
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-19 11:46:55 +0600

    a gym web app

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.scss';
+import AdvanceGym from './components/AdvanceGym/AdvanceGym/AdvanceGym';
+import Classes from './components/Classes/Classes/Classes';
+import Home from './components/Home/Home/Home';
+import Membership from './components/Membership/Membership/Membership';
+import Pricing from './components/Pricing/Pricing/Pricing';
+import NoMatch from './components/NoMatch/NoMatch';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+      <Router className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/classes">
+            <Classes></Classes>
+          </Route>
+          <Route path="/advanceGym">
+            <AdvanceGym></AdvanceGym>
+          </Route>
+          <Route path="/pricing">
+            <Pricing></Pricing>
+          </Route>
+          <Route path="/membership/:id">
+            <Membership></Membership>
+          </Route>
+          <Route path="*">
+            <NoMatch></NoMatch>
+          </Route>
+        </Switch>
+      </Router>
+  );
+}
+
+export default App;
